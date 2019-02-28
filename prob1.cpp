17b3dea09a035d69b9eaacee10b0a8d6423db53f
iliaspapa <45099764+iliaspapa@users.noreply.github.com>
2019-02-28 18:45:37 +0200

    Add files via upload

---
+++ prob1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<math.h>
+#include<cstring>
+#define forn for(int i=0;i<n;i++)
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define f first
+#define s second
+using namespace std;
+
+
